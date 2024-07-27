dc223428b6760f9d6ff914252be735b6ea5b0abf
DorukBüyüktanır <doruk.buyuktanir11@gmail.com>
2024-07-27 21:34:28 +0300

    GameLoop is better with no errors
    Exits are here to exit. You can return 0 if no erros or return 1 if there are errors in Update, LateUpdate and Awake.

--- PopPilot/PopPilot/GameEngine/GameLoop/FrameController.c
+++ PopPilot/PopPilot/GameEngine/GameLoop/FrameController.c
@@ -1,18 +1,34 @@
 #include "FrameController.h"
+#include "../ScreenStuff/Screen.h"
+
+int keyCodes;
 
 int main()
 {
+	int i = 0;
 	printf("Loading...:)");
 	SystemInfo systemInfo = newSystenInfo();
-	if(!functionEmpty(Awake))
-		Awake(systemInfo);
+	systemInfo.areWeChilling = true;
+
+	if(!functionEmpty(Awake)) {
+		keyCodes = Awake(systemInfo);
+		if (keyCodes == 0 || keyCodes == 1)
+			exit(keyCodes);
+	}
 
 	while (true) {
-		if (!functionEmpty(Update))
-			Update(systemInfo);
+		system("cls");
+		if (!functionEmpty(Update)) {
+			keyCodes = Update(systemInfo);
+			if (keyCodes == 0 || keyCodes == 1)
+				exit(keyCodes);
+		}
 
 
-		if (!functionEmpty(LateUpdate))
-			LateUpdate(systemInfo);
+		if (!functionEmpty(LateUpdate)) {
+			keyCodes = LateUpdate(systemInfo);
+			if (keyCodes == 0 || keyCodes == 1)
+				exit(keyCodes);
+		}
 	}
 }
--- PopPilot/PopPilot/PopPilot.c
+++ PopPilot/PopPilot/PopPilot.c
@@ -6,13 +6,17 @@
 
 
 int Update(SystemInfo sysInfo) {
-
+	char pressed = getch();
+	putchar(pressed);
+	if (pressed == 'q')
+		return 0;
+	return 2;
 }
 
 int LateUpdate(SystemInfo sysInfo) {
-
+	return 2;
 }
 
 int Awake(SystemInfo sysInfo) {
-
+	return 2;
 }
